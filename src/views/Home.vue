9c81e47c57fb7b73b0ce4b0a53479252117b3e5b
michael-mb <mbonimichael@yahoo.fr>
2021-06-10 22:40:03 +0200

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,22 @@
 <template>
   <div id="wrapper">
-    <Header></Header>
-    <section>
-      <div id="line">
-        <div class="dline"></div>
-        <h1>NEWS</h1>
-        <div class="dline"></div>
-      </div>
+      <Header></Header>
+      <section>
+          <div id="line">
+              <div class="dline"></div>
+                  <h1>NEWS</h1>
+              <div class="dline"></div>
+          </div>
 
-      <div id="ourserv">
-        <Article v-for="article in articles" :key="article.id" :title="article.title"
-                 :image-url="article.imageUrl"
-                 :article-url="article.articleUrl"
-                 :description="article.description"
-                  :date="article.date">
-        </Article>
-
-
-
-      </div>
-    </section>
+          <div id="ourserv">
+              <Article v-for="article in articles" :key="article.id" :title="article.title"
+                       :image-url="article.imageUrl"
+                       :article-url="article.articleUrl"
+                       :description="article.description"
+                        :date="article.date">
+              </Article>
+          </div>
+      </section>
 
     <Footer></Footer>
 
@@ -33,42 +30,40 @@ import Article from "../components/Article";
 import axios from "axios";
 export default {
 name: "Home",
-  components: {Article, Footer, Header},
-  data(){
-    return{
-      apiKey : "0b03ebb024eb489db4da7be90962a42f",
-      articles: []
-    }
-  },
-  created() {
+    components: {Article, Footer, Header},
+    data(){
+        return{
+            apiKey : "0b03ebb024eb489db4da7be90962a42f",
+            articles: []
+        }
+    },
+    created() {
+        let currentDate = this.getCurrentDate();
+        axios.get('https://newsapi.org/v2/everything?q=Apple&from='+currentDate+'&sortBy=popularity&apiKey='+this.apiKey)
+            .then((response => {
+                this.articles = [];
+                let fetchArticles = response.data.articles;
 
-    let currentDate = this.getCurrentDate();
-    axios.get('https://newsapi.org/v2/everything?q=Apple&from='+currentDate+'&sortBy=popularity&apiKey='+this.apiKey)
-        .then((response => {
-          this.articles = [];
-          let fetchArticles = response.data.articles;
-
-          for(let i=0 ; i < fetchArticles.length; i++){
-            this.articles.push({
-              description: fetchArticles[i].description,
-              imageUrl: fetchArticles[i].urlToImage,
-              articleUrl: fetchArticles[i].url,
-              title: fetchArticles[i].title,
-              date: fetchArticles[i].publishedAt,
-            })
-          }
-        }));
-  },
+                for(let i=0 ; i < fetchArticles.length; i++){
+                  this.articles.push({
+                    description: fetchArticles[i].description,
+                    imageUrl: fetchArticles[i].urlToImage,
+                    articleUrl: fetchArticles[i].url,
+                    title: fetchArticles[i].title,
+                    date: fetchArticles[i].publishedAt,
+                  })
+                }
+            }));
+    },
   methods:{
-      getCurrentDate(){
-        let today = new Date();
-        let dd = String(today.getDate()).padStart(2, '0');
-        let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-        let yyyy = today.getFullYear();
-
-        return  yyyy+'-'+mm+'-'+dd;
-      }
-  },
+        getCurrentDate(){
+            let today = new Date();
+            let dd = String(today.getDate()).padStart(2, '0');
+            let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+            let yyyy = today.getFullYear();
+            return  yyyy+'-'+mm+'-'+dd;
+        }
+    },
 }
 </script>
 
